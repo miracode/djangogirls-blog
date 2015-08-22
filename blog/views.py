8da47cab2e5b1367f7b7927de1b1eeccc9cd8557
miracode <rascatim@gmail.com>
2015-08-21 22:37:18 -0700

    changing colors, adding tabs

--- blog/views.py
+++ blog/views.py
@@ -51,3 +51,35 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
 
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
+
+
+def code_view(request):
+    return render(request, 'blog/code.html')
+
+
+def about_view(request):
+    return render(request, 'blog/about.html')
+
+
+def contact_view(request):
+    return render(request, 'blog/contact.html')
